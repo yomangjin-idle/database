25fde56ee4e304231f04895189cf03ab1d39f834
rkdaudwn26 <rkdaudwn26@daum.net>
2024-02-28 17:14:51 +0000

    chore : init.sql 수정

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -51,26 +51,3 @@ CREATE TABLE IF NOT EXISTS `yomangjin`.`speak_file` (
   `file_path` VARCHAR(255) COLLATE 'utf8mb3_bin' NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
-
-# INSERT INTO `yomangjin`.`tour` (`address`, `content`, `name`) VALUES ('제주시 봉개동 237-2번지, 명림로430', '제주 4·3 평화공원은 희생자들의 명예를 회복하고 제주도를 평화와 인권의 섬으로 만들기 위해 2008년 조성되었습니다. 평화공원에는 제주 4·3 평화기념관, 위령제단, 위령탑, 각명비, 행방불명인표석, 유해봉안관 등과 조형물들이 있으며, 매년 4월 3일에는 4·3 추념식이 열립니다.', '제주 4·3 평화공원');
-# INSERT INTO `yomangjin`.`tour` (`address`, `content`, `name`) VALUES ('제주시 삼도2동 관덕로 19', '관덕정은 1448년에 군사훈련을 위해 세워진 곳으로, 후에는 공사 의논과 죄인 처벌 등 다양한 목적으로 사용되었습니다. 1947년 3·1절 기념대회 때, 경찰이 시위중인 군중에게 발포하면서 6명이 희생되고 8명이 부상한 3·1 발포사건이 이곳에서 발생했습니다. 이 사건은 이후 제주 4·3 사건으로 이어지는 민관총파업의 발단이 되었습니다. 해방 직후에는 관덕정에서 건국준비위원회인 건준청년동맹이 활동했으며, 이후 인민위원회사무실로 이동했습니다. 현재는 당시 건물들이 대부분 사라져 역사의 흔적을 찾기 어렵습니다. ', '관덕정');
-# INSERT INTO `yomangjin`.`tour` (`address`, `content`, `name`) VALUES ('제주특별자치도 서귀포시 안덕면 사계리', '세화리 다랑쉬굴은 제주 4.3 사건 당시 11명의 주민이 피신하여 생활하다가 군경토벌대에 의해 발견되어 학살당한 장소입니다. 굴 입구에 불을 피워 연기를 불어넣어 주민들을 고통스럽게 죽였습니다. 1992년에 이 굴에서 11구의 유해가 발견되어 제주 4.3 사건의 진상을 규명하는 데 중요한 계기가 되었습니다. 유해는 발견된 지 45일 후에 화장되어 바다에 뿌려졌고, 이후 다랑쉬 굴은 유물들을 내부에 남겨두고 입구를 콘크리트로 막아 봉합되었습니다.', '다랑쉬굴');
-# INSERT INTO `yomangjin`.`tour` (`address`, `content`, `name`) VALUES ('제주시 화북 1동 4438', '1949년 1월 5일과 6일, 곤을동은 군인들에 의해 폐동이 되었습니다. 군인들이 마을을 포위하고 들어와 주민들을 수색하며, 젊은 사람들 10여 명을 고른 후 바닷가로 데려가 살해하였습니다. 이어서 마을의 주택들을 불태웠고, 이 과정은 다음 날에도 계속되었습니다. 젊은 주민들 12명을 추가로 모아 살해하며, 남아있던 집들도 모두 불태웠습니다. 이로써 67호가 넘는 집들이 한꺼번에 사라져 곤을동의 흔적은 사라졌습니다. 살아남은 주민들은 주변 마을로 옮겨져야 했습니다. 현재는 곤을동의 흔적이 별도봉 동편 기슭에 잘 남아있습니다.', '곤을동 옛터');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주시 조천읍 북촌3길 3', '너븐숭이 기념관');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주특별자치도 서귀포시 성산읍 고성리 224-1', '터진목');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('서귀포시 표선면 표선리 464-3', '한모살');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('서귀포시 남원읍 중산간동로 6242-56', '현의합장묘');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('서귀포시 남원읍 의귀리 1974-3번지', '송령이골');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('서귀포시 동홍동 칠십리로214번길 37', '정방폭포');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주특별자치도 서귀포시 대정읍 상모리 586-1', '백조일손지지');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주시 한림읍 월령리 381, 월령1길 22', '진아영 할머니 삶터');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주시 애월읍 어음리 1391', '빌레못굴');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주시 애월읍 하귀1리 1130-1번지', '영모원');
-# INSERT INTO `yomangjin`.`tour` (`address`, `name`) VALUES ('제주시 공항로 2 (제주시 용담동, 도두동 일대)', '정뜨르 비행장');
-
-# INSERT INTO `yomangjin`.`near_tour` (`dis`, `tour_id`, `address`, `content`, `name`) VALUES (2, 1, '제주특별자치도 제주시 관덕로 10', '제주 4.3 사건의 중심지', '관덕정 ');
-# INSERT INTO `yomangjin`.`near_tour` (`dis`, `tour_id`, `address`, `content`, `name`) VALUES (3, 1, '제주특별자치도 제주시 새연교길', '제주 4.3 사건 당시 많은 제주도민들이 희생된 장소', '새연교');
-# INSERT INTO `yomangjin`.`near_tour` (`dis`, `tour_id`, `address`, `content`, `name`) VALUES (2, 2, '제주특별자치도 제주시 오라로 2653', '제주 4.3 사건의 희생자를 기리는 공간', '제주 4.3 평화공원');
-# INSERT INTO `yomangjin`.`near_tour` (`dis`, `tour_id`, `address`, `content`, `name`) VALUES (3, 2, '제주특별자치도 제주시 새연교길', '제주 4.3 사건 당시 많은 제주도민들이 희생된 장소', '새연교 ');
-# INSERT INTO `yomangjin`.`near_tour` (`dis`, `tour_id`, `address`, `content`, `name`) VALUES (10, 3, '제주특별자치도 제주시 오라로 2653', '제주 4.3 사건의 희생자를 기리는 공간', '제주 4.3 평화공원');
-# INSERT INTO `yomangjin`.`near_tour` (`dis`, `tour_id`, `address`, `content`, `name`) VALUES (8, 3, '제주특별자치도 제주시 관덕로 10', '제주 4.3 사건의 중심지', '관덕정');
